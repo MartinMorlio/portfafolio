ff368a7650d35545cb047538ee3e293d843ea194
Martin Morlio <tincho20012017@gmail.com>
2023-01-28 13:24:00 -0300

    navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "252105f592d7292b"; }
+/******/ 		__webpack_require__.h = function() { return "f846161b033f0bd8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,15 @@
 const Navbar = () => {
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">
-          Portafolio
-        </a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link href="/">
+          <a className="navbar-brand">Simple Portfolio</a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -16,28 +17,23 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link href="/blog">
+                <a className="nav-link active" aria-current="page">
+                  Blog
+                </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
+              <Link href="/github">
+                <a className="nav-link">Github</a>
+              </Link>
             </li>
           </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
