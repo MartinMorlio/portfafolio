ad4e0c4498ed7dd9fcc6d559c320af491377777d
Martin Morlio <tincho20012017@gmail.com>
2023-01-28 13:14:29 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+// Importamos css de toda la app o funcionalidad relacionada con la pag.
+// Es como una parte por fuera del body, configuración de la app.
+import 'bootswatch/dist/lux/bootstrap.min.css';
+
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const Index = () => {
+  return (
+    <>
+    <Layout>
+      <h1>Homeeeeeee</h1>
+    </Layout>
+    </>
+  );
+};
+
+export default Index;
